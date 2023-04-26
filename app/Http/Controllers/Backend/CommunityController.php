5edb98a1e456cfdc8f483e653fdf1131e6ff2cad
Aslonov Ismoil <ismoilcode@gmail.com>
2023-04-27 01:30:16 +0200

    display all communities

--- app/Http/Controllers/Backend/CommunityController.php
+++ app/Http/Controllers/Backend/CommunityController.php
@@ -15,7 +15,12 @@ class CommunityController extends Controller
      */
     public function index()
     {
-        return "ok";
+        $communities = Community::where('user_id', auth()->id())->paginate(5)->through(fn ($community) => [
+            'id' => $community->id,
+            'name' => $community->name,
+            'slug' => $community->slug,
+        ]);
+        return Inertia::render('Communities/Index', compact('communities'));
     }
 
     /**
